7a9603d72bb31b3bfe02cb3ed3345a5dedf5c65d
JasmineQian <jasmine.qian@liveramp.com>
2022-07-11 15:24:13 +0800

    first commit

---
+++ 111.py
@@ -0,0 +1,11 @@
+from time import sleep
+
+if __name__ == '__main__':
+    print("Hello AE Shcedule")
+
+    print("I am going to sleeping for 300 seconds.")
+    sleep(600)
+
+    print("I wake up after 300 seconds.")
+
+    print("exsiting")
---
+++ README.md
@@ -0,0 +1,1 @@
+# codeSync-test01
